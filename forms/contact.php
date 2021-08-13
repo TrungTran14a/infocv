4e24a45910c2a413b07fd403465df43bbf8cb727
TrungTran14a <trung199622@gmail.com>
2021-08-14 01:09:25 +0700

    more js

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,50 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+ 
+    // Nếu không phải là sự kiện đăng ký thì không xử lý
+    
+     
+    //Nhúng file kết nối với database
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    include('ketnoibh.php');
+          
+    //Khai báo utf-8 để hiển thị được tiếng việt
+    header('Content-Type: text/html; charset=UTF-8');
+          
+    //Lấy dữ liệu từ file menuside.php
+    $a    = addslashes($_POST['txtname']);
+    $b    = addslashes($_POST['txtemail']);
+    $c    = addslashes($_POST['txtsubject']);
+    $d    = addslashes($_POST['txtmessage']);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    //Kiểm tra người dùng đã nhập liệu đầy đủ chưa
+    if (!$a || !$b || !$c || !$d)
+    {
+        echo "Vui lòng nhập đầy đủ thông tin. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        echo 
+        "name:".$a,
+        "mail:".$b,
+        "subject:".$c,
+        "message:".$d,
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+        exit;
+    }
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+     //Lưu thông tin thành viên vào bảng
+     @$addreq = mysqli_query($conn,
+     " INSERT INTO request (
+                            name,
+                            mail,
+                            subject,
+                            message
+                            )
+     VALUE (
+         
+         '{$a}',
+         '{$b}',
+         '{$c}',
+         '{$d}'
+     )"
+ );
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+	?>
---
+++ forms/ketnoibh.php
@@ -0,0 +1,22 @@
+
+
+ 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo("connect success");
+}
+
+mysqli_select_db($conn , "infocv");
+
+		
+?> 
